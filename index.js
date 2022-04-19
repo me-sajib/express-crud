bb3a243bbcc38b054b22d37059b6a23ba56e1a61
sajib <me.mrsajib@gmail.com>
2022-04-19 18:08:12 +0600

    mongodb connect successfully

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// monogoDB connection method
+const uri =
+  "mongodb+srv://products:vDMB6oaIVDgU3m6s@cluster0.p0jku.mongodb.net/productsdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 const app = express();
-const password = "vDMB6oaIVDgU3m6s";
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// mongodb connect
+client.connect((err) => {
+  const collection = client.db("productsdb").collection("products");
+  // perform actions on the collection object
+  console.log("Connected to MongoDB");
+  client.close();
+});
+
+// run the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
