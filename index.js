9c6587a0c92c3ad6ddc0d2a427d451a24f7b61f2
sajib <me.mrsajib@gmail.com>
2022-04-19 18:02:19 +0600

    install mongodb, middleware and run the server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const password = "vDMB6oaIVDgU3m6s";
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
